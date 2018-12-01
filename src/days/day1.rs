231fc16b2c556fe1f918fdf046b121fecae06743
Jeremy Knope <me@jeremyknope.com>
2018-12-01 12:37:24 -0500

    Fancy up solutions into a clap.rs powered binary, with days as modules

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,11 +1,10 @@
-use std::fs::File;
-use std::io::{BufReader};
-use std::io::prelude::*;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-#[allow(dead_code)]
 fn step1() {
-    let f = File::open("input.txt").expect("file not found");
+    let f = File::open("input/day1/input.txt").expect("file not found");
     let f = BufReader::new(f);
     let mut freq = 0;
     for line in f.lines() {
@@ -17,7 +16,7 @@ fn step1() {
 }
 
 fn step2(frequency: i32, previous: &mut HashMap<i32, i32>) -> (i32, Option<i32>) {
-    let f = File::open("input.txt").expect("file not found");
+    let f = File::open("input/day1/input.txt").expect("file not found");
     let f = BufReader::new(f);
     let mut frequency = frequency;
     for line in f.lines() {
@@ -26,26 +25,29 @@ fn step2(frequency: i32, previous: &mut HashMap<i32, i32>) -> (i32, Option<i32>)
         frequency += my_int;
         if previous.contains_key(&frequency) {
             return (frequency, Some(frequency));
-        }
-        else {
+        } else {
             previous.insert(frequency, 1);
         }
     }
     (frequency, None)
 }
 
-fn main() {
-    // step1();
-    let mut mymap: HashMap<i32, i32> = HashMap::new();
-    let mut prev_freq = 0;
-    let (f, _) = step2(prev_freq, &mut mymap);
-    prev_freq = f;
-    loop {
-        let (f, dupe) = step2(prev_freq, &mut mymap);
+pub fn run(step: u8) {
+    if step == 1 {
+        step1();
+    }
+    else {
+            let mut mymap: HashMap<i32, i32> = HashMap::new();
+        let mut prev_freq = 0;
+        let (f, _) = step2(prev_freq, &mut mymap);
         prev_freq = f;
-        if let Some(d) = dupe {
-            println!("Dupe: {}", d);
-            return;
+        loop {
+            let (f, dupe) = step2(prev_freq, &mut mymap);
+            prev_freq = f;
+            if let Some(d) = dupe {
+                println!("Answer: {}", d);
+                return;
+            }
         }
     }
 }
